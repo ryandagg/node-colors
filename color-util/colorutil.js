bb035a52a74e3170c2f93cd2a0d986519a3073b6
ryandagg <ryan.dagg@gmail.com>
2014-07-21 16:53:01 -0600

    added darken by 20% luminosity method

--- color-util/colorutil.js
+++ color-util/colorutil.js
@@ -2,6 +2,12 @@ var luminosity = function(r, g, b) {
 	return 0.2126*r + 0.7152*g + 0.0722*b;
 }
 
+var darken = function(r, g, b) {
+	// console.log(r*0.8, g*0.8, b*0.8);
+	return String(Math.round(r*0.8 * 10) / 10) + ", " + String(Math.round(g*0.8 * 10) / 10)+ ", " + String(Math.round(b*0.8 * 10) / 10);
+}
+
 module.exports = {
-	luminosity: luminosity
+	luminosity: luminosity,
+	darken: darken
 };
